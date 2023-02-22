08caced31b52bcc1b3035ab93fa3cf8ce5d8d881
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2023-02-22 14:44:02 -0800

    Remove imports

--- src/me/neoblade298/base/Main.java
+++ src/me/neoblade298/base/Main.java
@@ -3,9 +3,6 @@ package me.neoblade298.base;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.neoblade298.neocore.bukkit.commands.SubcommandManager;
-import net.md_5.bungee.api.ChatColor;
-
 public class Main extends JavaPlugin {
 	private static Main inst;
 	
